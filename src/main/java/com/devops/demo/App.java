68fb5062f13ae2d02597d0238dc8f0d3a770c11c
ranadeepmr <ranadeepmr@gmail.com>
2018-03-13 18:05:56 +0800

    Hai
    sample

---
+++ src/main/java/com/devops/demo/App.java
@@ -0,0 +1,18 @@
+package com.devops.demo;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class App {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
